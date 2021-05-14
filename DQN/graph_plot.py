7761f9bed2966add48e14a938bc3728c68dd4031
Duncan <41350149+aloko001@users.noreply.github.com>
2021-05-14 14:50:14 +0100

    DQN_models

---
+++ DQN/graph_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import os 
+
+if __name__ == '__main__':
+
+
+    data_path = ''
+    results = {}
+    num_seeds = {}
+
+    
---
+++ DQN/hyperparameters.py
@@ -0,0 +1,8 @@
+env = ForexEnv()
+
+RL_agent = DQNAgent(
+    learning_rate=
+    batch_size=
+    buffer_size=
+    num_batches=
+)

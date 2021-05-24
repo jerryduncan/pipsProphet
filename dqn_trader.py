3d9ff61e3f1fea900228437f1ca6ae67c777b496
Duncan <41350149+aloko001@users.noreply.github.com>
2021-05-24 08:22:43 +0100

    Update dqn_trader.py

--- dqn_trader.py
+++ dqn_trader.py
@@ -19,7 +19,25 @@ from TraderView import OhlcvEnv
 from util import Normalizerprocessor 
 
 #ultulizing trader environment
+def model_create(shape, nb_actions):
+    model = Sequential()
+    return model
 
+def main():
+    ENV_NAME = 'OHLCV'
+    TIME_STEP = 30
+
+    TRAIN_PATH = ""
+    TEST_PATH = ""
+    env_train = OhlcvEnv(TIME_STEP, path=TRAIN_PATH)
+    env_test = OhlcvEnv(TIME_STEP, path=TEST_PATH)
+
+    np.random.seed(456)
+    env.seed(562)
+
+    nb_actions = env.action_space.n
+    model = model_create(shape=env.shape, nb_actions=nb_actions)
+    print(model.summary())
 
 if __name__ == '__main__':
     main()

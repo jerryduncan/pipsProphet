c800c9d624e6b73381e96632fdbdbdfefe97ba29
Duncan <41350149+aloko001@users.noreply.github.com>
2021-05-30 08:39:57 +0100

    initialize agent

--- DQN/agents/__init__.py
+++ DQN/agents/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 from .dqn import DQNAgent, NAFAgent, ContinuousDQNAgent
 from .ddpg import DDPGAgent
-from .cem import CEMAgent
+#from .cem import CEMAgent
 from .sarsa import SarsaAgent, SARSAAgent
